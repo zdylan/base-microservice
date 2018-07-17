611564e31092118bf2c2bc6d889533b0774e91d5
zhang.gai <gai.zhang@nx-engine.com>
2018-07-17 23:13:47 +0800

    add base-eureka and base-config.

---
+++ base-config/src/main/java/com/dylan/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.dylan.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dylan on 2018/7/17.
+ */
+
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
